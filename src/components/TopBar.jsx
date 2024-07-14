fa6a2ecf1dd1f13b36cc1d2d177620f98992ec89
SALEHIN <msionlinekingdom@gmail.com>
2024-07-14 23:27:18 +0600

    notification signal change

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -41,7 +41,7 @@ const TopBar = ({portal}) => {
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle mr-4">
         <div className="indicator tooltip tooltip-bottom">
-          <span className="loading loading-ring loading-sm indicator-item badge font-bold badge-neutral text-red-500"></span>
+          <span className="loading loading-ring loading-sm indicator-item badge font-bold badge-neutral text-white"></span>
           <div className=" grid place-items-center text-black">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
           <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
