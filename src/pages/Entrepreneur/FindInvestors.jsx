4ede0d5b110131286a4f63196b6174b5da2bccc1
Tanvir-Chowdhury <tanvirvlogger@gmail.com>
2024-07-15 22:49:40 +0600

    message

--- src/pages/Entrepreneur/FindInvestors.jsx
+++ src/pages/Entrepreneur/FindInvestors.jsx
@@ -31,7 +31,7 @@ const FindInvestors = () => {
 
   return (
 
-    <div className="bg-gray-950 text-gray-300 ml-40 h-full">
+    <div className="bg-gray-950 text-gray-300 ml-40 ">
       <div className="container mx-auto pb-7">
         <h1 className='text-center text-4xl font-bold text-[#d4a1e9] py-7'>Find Investors</h1>
 
