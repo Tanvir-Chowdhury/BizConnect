5c158fb6f3838bbd4def12c74be784a2c91c2927
SALEHIN <msionlinekingdom@gmail.com>
2024-07-16 12:43:46 +0600

    bannar done

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,26 @@ const App = () => {
             <img src="../public/logo/BizConnect.png" alt="Logo" className="w-24 sm:w-28 md:w-32"></img>
           </button>
         </div>
-        <nav className="flex items-center space-x-1 text-sm font-medium text-gray-800">
-          <button className="rounded-xl bg-[#0000003b] px-5 py-2 border border-gray-300 text-white transition hover:bg-violet-900"> Login -<span className='text-gray-300'>It's Free</span> </button>
+        <nav className="flex items-center space-x-1 text-sm font-medium text-gray-800 gap-4">
+          <button className="rounded-xl bg-[#0000003b] px-5 py-2 border border-gray-300 text-white transition hover:bg-violet-900"> Login </button>
+          <button className="rounded-xl bg-[#0000003b] px-5 py-2 border border-gray-300 text-white transition hover:bg-violet-900"> Sign Up </button>
         </nav>
       </div>
       
       {/* banner */}
-      <div className=""></div>
+      <div className="flex flex-col justify-center space-y-8 py-10">
+        <h3 className='text-3xl text-center'>Welcome to <span className='text-violet-300 font-bold'>Biz</span><span className='text-pink-200 font-bold'>Connect</span></h3>
+        <h1 className='text-5xl text-center bg-gradient-to-tl from-[#e26df0] to-[#fa74ad] inline-block text-transparent bg-clip-text'>Connecting <b>Student</b>, <b>Entrepreneur</b>, <b>Investor</b></h1>
+        <h4 className='text-center text-lg'>We believe in connection for your Empowerment</h4>
+        {/* <img src="/public/Home/bannar-foot.webp" alt="" className='bottom-0' /> */}
+        <div className=" flex flex-col justify-center space-y-5" style={{backgroundImage: `url(/public/Home/bannar-foot.webp)`, backgroundRepeat:`no-repeat`, backgroundSize:`cover`}}>
+          <h3 className='text-center text-xl'>So why be late? Connect with the Future Maker for free!</h3>
+          <div className=" flex flex-row gap-10 justify-center">
+          <button className="rounded-xl bg-[#0000003b] px-5 py-2 border border-gray-300 text-white transition hover:bg-violet-900"> Login </button>
+          <button className="rounded-xl bg-[#0000003b] px-5 py-2 border border-gray-300 text-white transition hover:bg-violet-900"> Sign Up </button>
+          </div>
+        </div>
+      </div>
     </div>
     </>
   ); 
