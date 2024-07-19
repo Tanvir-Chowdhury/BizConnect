44df8606ad17b2e262c5e83d0c4d4d7d4677ec1d
ay0-0n <sartizayo.on@gmail.com>
2024-07-19 11:24:24 +0600

    Login Modification

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,37 @@
-// pages/Login.js
-import { useState } from 'react';
-import { useContext } from 'react';
-// import { AuthContext } from '../context/authContext';
-
-import logo from '../../public/logo/BizConnect.png'; // Replace with your actual logo file path
+import { Link } from 'react-router-dom';
+import logo from '../../public/logo/BizConnect.png'; 
+import { useContext, useState } from 'react';
+import { AuthContext } from '../auth/AuthProvider';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [role, setRole] = useState('entrepreneur'); // Default role
+  const { logInWithEmailPass} = useContext(AuthContext)
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  
 
-  //   const { login } = useContext(AuthContext);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Call API to authenticate user
-    //...
-    // const user = { id: 1, role: 'investor' }; // Replace with API response
-    // login(user, user.role);
+  const handleSubmit = async(e) => {
+    setLoading(true)
+    e.preventDefault();
+    const email = e.target.email.value
+    const pass = e.target.password.value
+    
+    logInWithEmailPass(email, pass)
+        .then(() => {
+          setError(false);
+          setLoading(false)
+          //if data-> dashboard
+          //else -> info
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading(false)
+        });
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-700">
       <div className="bg-gray-900 p-8 rounded-lg shadow-lg">
-        <img src={logo} alt="Website Logo" className="h-16 mb-4 mx-auto" /> {/* Logo */}
+        <Link to='/'><img src={logo} alt="Website Logo" className="h-16 mb-4 mx-auto" /></Link> {/* Logo */}
         <h2 className="text-3xl font-bold text-center text-white mb-6">Login</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div className="flex items-center justify-center space-x-4">
@@ -33,10 +41,9 @@ const Login = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
               </svg>
               <input
-                type="text"
-                value={username}
-                onChange={(event) => setUsername(event.target.value)}
-                placeholder="Username"
+                type="email"
+                placeholder="Email"
+                name='email'
                 className="bg-transparent focus:outline-none ml-2 text-white"
               />
             </div>
@@ -47,17 +54,17 @@ const Login = () => {
               </svg>
               <input
                 type="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
                 placeholder="Password"
+                name='password'
                 className="bg-transparent focus:outline-none ml-2 text-white"
               />
             </div>
           </div>
+          {error && <div className='w-full flex justify-center items-center text-center text-sm text-red-500'>{error} <br /> Please try again</div>}
           {/* Submit Button */}
           <div className="flex justify-center">
-            <button type="submit" className="bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded-md mt-4">
-              Login
+            <button type="submit" className="bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded-md mt-4 flex justify-center items-center">
+              {loading? (<span className="loading loading-ball loading-md"></span>):"Login"}
             </button>
           </div>
         </form>
